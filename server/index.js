a94d5736e0086365b3b416ab5f1ee11ee1b8b04e
Raphael Thomazella <thomazella9@gmail.com>
2018-09-28 21:09:10 -0300

    implement simple mutation query

--- server/index.js
+++ server/index.js
@@ -1,37 +1,41 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { makeExecutableSchema } from 'graphql-tools';
+import { ApolloServer, gql } from "apollo-server";
+import { makeExecutableSchema } from "graphql-tools";
 
-import * as BookType from './src/modules/book/BookType';
-import * as AuthorType from './src/modules/author/AuthorType';
+import * as BookType from "./src/modules/book/BookType";
+import * as AuthorType from "./src/modules/author/AuthorType";
 
 const SchemaDefinition = `
   schema {
     query: Query
+    mutation: Mutation
   }
   type Query {
     books: [Book]
     authors: [Author]
   }
+  type Mutation {
+    addBook(title: String!): Book
+  }
 `;
 
-const typeDefs = [
-  BookType.typeDefs,
-  AuthorType.typeDefs,
-]
+const typeDefs = [BookType.typeDefs, AuthorType.typeDefs];
 
 const resolvers = {
   Query: {
     ...BookType.resolvers,
     ...AuthorType.resolvers
   },
+  Mutation: {
+    addBook: (root, args, context) => {
+      return { title: args.title };
+    }
+  }
 };
-
-
 
 const schema = makeExecutableSchema({
   typeDefs: [SchemaDefinition, ...typeDefs],
   resolvers
-}) 
+});
 
 const server = new ApolloServer({ schema });
 
