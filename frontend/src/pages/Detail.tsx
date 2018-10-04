822c757dfad67a0895da91306d9014e7abaced87
Raphael Thomazella <thomazella9@gmail.com>
2018-10-04 13:44:24 -0300

    [front] refactor Detail

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -1,15 +1,21 @@
 import * as React from "react";
-import { Text, Image } from "react-native";
+import { Text } from "react-native";
 import styled from "styled-components/native";
 import Layout from "../layouts/DefaultLayout";
 import { Button } from "../components";
 
 const Title = styled.Text`
-    font-size: 21px;
-    color: darkblue;
-    margin-top: 25px;
-    margin-bottom: 25px;
-    text-align: center;
+  font-size: 21px;
+  color: darkblue;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  text-align: center;
+`;
+
+const Cover = styled.Image`
+  width: 220px;
+  height: 220px;
+  margin: 25px;
 `;
 
 const Wrapper = styled.View`
@@ -20,7 +26,7 @@ const Wrapper = styled.View`
   padding: 25px;
 `;
 
-const Body = styled.Text`
+const Description = styled.Text`
   font-size: 17px;
   margin-top: 25px;
   margin-bottom: 25px;
@@ -35,15 +41,14 @@ const Detail = (props: any) => {
     <Layout user={user}>
       <Wrapper>
         <Title>{book.title}</Title>
-        <Image
-          style={{ width: 220, height: 220, margin: 25 }}
+        <Cover
           source={{
             uri: "https://placekitten.com/220/220"
           }}
         />
-        <Body>
+        <Description>
           By {book.author.name}, {book.author.age} years old.
-        </Body>
+        </Description>
         <Button onPress={() => navigate("List", { user })}>
           <Text>Back</Text>
         </Button>
